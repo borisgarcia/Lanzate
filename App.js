d1a503c151e89fc611e6e8d7722704af9b660c04
brgp <brgp@live.com>
2019-11-03 21:07:54 -0600

    Lanzate app

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Platform, StatusBar, Image } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import { Block, GalioProvider } from "galio-framework";
+
+import AppContainer from "./navigation/Screens";
+import { Images, products, materialTheme } from "./constants/";
+
+// cache app images
+const assetImages = [
+  Images.Pro,
+  Images.Profile,
+  Images.Avatar,
+  Images.Onboarding
+];
+
+products.map(product => assetImages.push(product.image));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <GalioProvider theme={materialTheme}>
+          <Block flex>
+            {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+            <AppContainer />
+          </Block>
+        </GalioProvider>
+      );
+    }
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([...cacheImages(assetImages)]);
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
---
+++ constants/Images.js
@@ -0,0 +1,30 @@
+const Onboarding =
+  "https://www.revistaestilo.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=PKtIYQUCWbkUky8sQE1qqM$daE2N3K4ZzOUsqbU5sYu_edBIPdXbiw2zZjH7WvWyWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg";
+const Pro =
+  "https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80";
+const Products = {
+  Accessories: "https://source.unsplash.com//l1MCA0VyNrk/840x840"
+};
+
+const Profile =
+  "https://upload.wikimedia.org/wikipedia/commons/2/2c/El_Malecon_Puerto_Cortes_Honduras.jpg";
+const Avatar =
+  "https://images.unsplash.com/photo-1518725522904-4b3939358342?fit=crop&w=210&q=80";
+
+const Viewed = [
+  "https://images.unsplash.com/photo-1508264443919-15a31e1d9c1a?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1487376480913-24046456a727?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1494894194458-0174142560c0?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?fit=crop&w=240&q=80",
+  "https://images.unsplash.com/photo-1542068829-1115f7259450?fit=crop&w=240&q=80"
+];
+
+export default {
+  Onboarding,
+  Pro,
+  Products,
+  Profile,
+  Viewed,
+  Avatar
+};
